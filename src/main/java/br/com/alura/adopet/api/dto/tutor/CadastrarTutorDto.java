b3087bcb4058a76727f80c40491a3cf8e628155c
Gabriel <grodrigues256@gmail.com>
2023-11-24 08:57:24 -0300

    refactor(v2#-22-11-23) Melhorias no codigo part 2.

--- src/main/java/br/com/alura/adopet/api/dto/tutor/CadastrarTutorDto.java
+++ src/main/java/br/com/alura/adopet/api/dto/tutor/CadastrarTutorDto.java
@@ -1,6 +1,10 @@
 package br.com.alura.adopet.api.dto.tutor;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record CadastrarTutorDto(@NotBlank String nome, @NotBlank String telefone, @NotBlank String email) {
+public record CadastrarTutorDto(@NotBlank String nome,
+                                @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}") @NotBlank String telefone,
+                                @Email @NotBlank String email) {
 }
